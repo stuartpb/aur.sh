7a426e1290704fb63fe88d0381b91d762fd205cb
Stuart P. Bentley <stuart@testtrack4.com>
2013-04-16 01:51:15 -0700

    Code golf: use a shorter variable name for each package

--- aurso.sh
+++ aurso.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-for var
+for p
 do
 cd `mktemp -d`
-curl https://aur.archlinux.org/packages/${var:0:2}/$var/$var.tar.gz |tar xz
-cd $var
+curl https://aur.archlinux.org/packages/${p:0:2}/$p/$p.tar.gz |tar xz
+cd $p
 makepkg -si
 done
